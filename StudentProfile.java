65ee266d56a075d5bf07739f83543c8206a3b7f7
IvanBelovFSJ <38769259+IvanBelovFSJ@users.noreply.github.com>
2020-06-10 11:54:48 -0700

    Create StudentProfile.java

---
+++ StudentProfile.java
@@ -0,0 +1,12 @@
+
+public class StudentProfile
+{
+
+	public static void main(String[] args)
+	{
+		@SuppressWarnings("unused")
+		Student studentInfo = new Student();
+		System.out.println ( "\nThe app has closed.");
+	}
+
+}
